2c348afb95ce568ec0f1806ca9915f227c2b352c
Nikita Kyarnonen <nk47blue@gmail.com>
2025-03-26 18:56:54 +0000

    fix error mkdir /var/lib/jenkins: read-only file system with /tmp

--- src/org/security/scanners/ZAPScanner.groovy
+++ src/org/security/scanners/ZAPScanner.groovy
@@ -2,7 +2,8 @@ package org.security
 
 class ZAPScanner {
     static String runScan(String targetUrl = 'https://juiceshop.kyarnk.ru', String outputFile = 'zap_report.json') {
-        return "docker run --rm -v $(pwd):/zap/wrk/:rw -t zaproxy/zap-stable " +
-               "zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/${outputFile}"
+        return "docker run --rm -v /tmp:/zap/wrk/ -t zaproxy/zap-stable " +
+               "zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/zap_temp.json && " +
+               "cp /tmp/zap_temp.json ${outputFile}"
     }
 }
--- vars/runZAPScan.groovy
+++ vars/runZAPScan.groovy
@@ -1,6 +1,7 @@
 def call(String targetUrl = 'https://juiceshop.kyarnk.ru', String outputFile = 'zap_report.json') {
     sh """
-        docker run --rm -v \$(pwd):/zap/wrk/:rw -t zaproxy/zap-stable zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/${outputFile}
+        docker run --rm -v /tmp:/zap/wrk/ -t zaproxy/zap-stable zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/zap_temp.json
+        cp /tmp/zap_temp.json ${outputFile}
     """
     echo "OWASP ZAP scan completed. Report saved to ${outputFile}"
 }
