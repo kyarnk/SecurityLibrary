c85a1e7974c1d7c4be5ced67f687f658c7f913e0
Nikita Kyarnonen <nk47blue@gmail.com>
2025-03-27 16:10:26 +0000

    del cp /tmp and added homedir insta

--- src/org/security/scanners/ZAPScanner.groovy
+++ src/org/security/scanners/ZAPScanner.groovy
@@ -2,8 +2,7 @@ package org.security
 
 class ZAPScanner {
     static String runScan(String targetUrl = 'https://juiceshop.kyarnk.ru', String outputFile = 'zap_report.json') {
-        return "docker run --rm -v /tmp:/zap/wrk/ -t zaproxy/zap-stable " +
-               "zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/zap_temp.json && " +
-               "cp /tmp/zap_temp.json ${outputFile}"
+        return "docker run --rm -v /home/kyarnk/zap-reports:/zap/wrk/ -t zaproxy/zap-stable " +
+               "zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/${outputFile}"
     }
 }
--- vars/runZAPScan.groovy
+++ vars/runZAPScan.groovy
@@ -1,7 +1,7 @@
 def call(String targetUrl = 'https://juiceshop.kyarnk.ru', String outputFile = 'zap_report.json') {
     sh """
-        docker run --rm -v /tmp:/zap/wrk/ -t zaproxy/zap-stable zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/zap_temp.json
-        cp /tmp/zap_temp.json ${outputFile}
+        docker run --rm -v /home/kyarnk/zap-reports:/zap/wrk/ -t zaproxy/zap-stable \
+        zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/${outputFile}
     """
-    echo "OWASP ZAP scan completed. Report saved to ${outputFile}"
+    echo "OWASP ZAP scan completed. Report saved to /home/kyarnk/zap-reports/${outputFile}"
 }
