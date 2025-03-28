536a3f54e444be66e7f88f1d6966264bd0181f17
Nikita Kyarnonen <nk47blue@gmail.com>
2025-03-28 22:56:09 +0000

    fix semgrep work

--- src/org/security/scanners/SemgrepScanner.groovy
+++ src/org/security/scanners/SemgrepScanner.groovy
@@ -2,6 +2,8 @@ package org.security
 
 class SemgrepScanner {
     static String runScan(String targetDir = '/home/kyarnk/juice-shop', String outputFile = 'semgrep_report.json') {
-        return "docker run --rm -v ${targetDir}:/src -v /home/kyarnk/semgrep-reports:/output docker.io/semgrep/semgrep:latest semgrep scan --config auto --json -o /output/${outputFile}"
+        return """
+            docker run --rm -v ${targetDir}:/src docker.io/semgrep/semgrep:latest semgrep scan --config auto --json > /home/kyarnk/semgrep-reports/${outputFile}
+        """
     }
 }
--- vars/runSemgrepScan.groovy
+++ vars/runSemgrepScan.groovy
@@ -1,6 +1,6 @@
 def call(String targetDir = '/home/kyarnk/juice-shop', String outputFile = 'semgrep_report.json') {
     sh """
-        docker run --rm -v ${targetDir}:/src -v /home/kyarnk/semgrep-reports:/output docker.io/semgrep/semgrep:latest semgrep scan --config auto --json > /output/${outputFile}
+        docker run --rm -v ${targetDir}:/src docker.io/semgrep/semgrep:latest semgrep scan --config auto --json > /home/kyarnk/semgrep-reports/${outputFile}
     """
     echo "Semgrep scan completed. Report saved to /home/kyarnk/semgrep-reports/${outputFile}"
 }
