090f0d46903ba06fe74d2416ef9386272bffc980
Nikita Kyarnonen <nk47blue@gmail.com>
2025-03-28 19:58:50 +0000

    added dir for output semgrep

--- vars/runSemgrepScan.groovy
+++ vars/runSemgrepScan.groovy
@@ -1,6 +1,6 @@
-def call(String targetDir = '.', String outputFile = 'semgrep_report.json') {
+def call(String targetDir = '.', String outputDir = 'semgrep-reports', String outputFile = 'semgrep_report.json') {
     sh """
-        docker run --rm -v ${targetDir}:/src docker.io/semgrep/semgrep:latest semgrep scan --config auto --json | tee ${outputFile}
+        docker run --rm -v ${targetDir}:/src -v ${outputDir}:/output docker.io/semgrep/semgrep:latest semgrep scan --config auto --json | tee /output/${outputFile}
     """
-    echo "Semgrep scan completed. Report saved to ${outputFile}"
+    echo "Semgrep scan completed. Report saved to ${outputDir}/${outputFile}"
 }
