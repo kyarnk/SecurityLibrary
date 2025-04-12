f0fcd72ba414aed30afcbff9b17da3e1ae8d95ff
Nikita Kyarnonen <nk47blue@gmail.com>
2025-04-12 13:48:52 +0000

    semgrep

--- src/org/security/scanners/SemgrepScanner.groovy
+++ src/org/security/scanners/SemgrepScanner.groovy
@@ -1,7 +1,7 @@
 package org.security
 
 class SemgrepScanner {
-    static String runScan(String targetDir = '', String outputFile = 'semgrep_report.json') {
+    static String runScan(String targetDir = '/home/kyarnk', String outputFile = 'semgrep_report.json') {
         return """
             docker run --rm -v ${targetDir}:/mnt semgrep/semgrep semgrep --config=auto /mnt/JenkinsSecurity/juice-shop -o /mnt/reports/${outputFile}
         """
--- vars/runSemgrepScan.groovy
+++ vars/runSemgrepScan.groovy
@@ -1,15 +1,7 @@
-// def call(String targetDir = '', String outputFile = 'semgrep_report.json') {
-//     sh """
-//         docker run --rm -v /var/lib/jenkins:/mnt semgrep/semgrep semgrep --config=auto /mnt/JenkinsSecurity/juice-shop -o /mnt/reports/${outputFile}
-//     """
-//     echo "Semgrep scan completed. Report saved to /var/lib/jenkins/reports/${outputFile}"
-// }
-
-def call(String scanPath = '.', String outputFile = 'semgrep_report.json') {
-    def reportDir = 'reports'
+def call(String targetDir = '', String outputFile = 'semgrep_report.json') {
     sh """
-        mkdir -p ${reportDir}
-        semgrep --config=auto ${scanPath} -o ${reportDir}/${outputFile}
+        docker run --rm -v /home/kyarnk/jenkins:/mnt semgrep/semgrep semgrep --config=auto /mnt/JenkinsSecurity/juice-shop -o /mnt/reports/${outputFile}
     """
-    echo "✅ Semgrep scan completed. Report saved to ${reportDir}/${outputFile}"
+    echo "Semgrep scan completed. Report saved to /var/lib/jenkins/reports/${outputFile}"
 }
+
