305fd14ca73d52da732acd371af9465824eb5774
Nikita Kyarnonen <nk47blue@gmail.com>
2025-04-12 20:02:46 +0000

    added zap

--- vars/runZAPScan.groovy
+++ vars/runZAPScan.groovy
@@ -1,7 +1,24 @@
-def call(String targetUrl = 'https://juiceshop.kyarnk.ru', String outputFile = 'zap_report.json') {
+// def call(String targetUrl = 'https://juiceshop.kyarnk.ru', String outputFile = 'zap_report.json') {
+//     sh """
+//         docker run --rm -v /home/kyarnk/zap-reports:/zap/wrk/ -t zaproxy/zap-stable \
+//         zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/${outputFile}
+//     """
+//     echo "OWASP ZAP scan completed. Report saved to /home/kyarnk/zap-reports/${outputFile}"
+// }
+
+def call(String targetUrl = '', String outputFile = 'zap_report.json', String homeDir = '') {
+    if (!targetUrl || !homeDir) {
+        error "Both targetUrl and homeDir must be specified!"
+    }
+
+    def outputDir = "${homeDir}/reports"
+    sh "mkdir -p ${outputDir}"
+
+    // Запуск ZAP в headless-режиме (quick scan)
     sh """
-        docker run --rm -v /home/kyarnk/zap-reports:/zap/wrk/ -t zaproxy/zap-stable \
+        docker run --rm -v ${outputDir}:/zap/wrk/ -t zaproxy/zap-stable \
         zap.sh -cmd -quickurl ${targetUrl} -quickout /zap/wrk/${outputFile}
     """
-    echo "OWASP ZAP scan completed. Report saved to /home/kyarnk/zap-reports/${outputFile}"
+
+    echo "OWASP ZAP scan completed. Report saved to ${outputDir}/${outputFile}"
 }
