f8e76b11b82c66cf529422c348f3d4cdb9fd31cb
Nikita Kyarnonen <nk47blue@gmail.com>
2025-04-12 13:52:26 +0000

    semgrep

--- vars/runSemgrepScan.groovy
+++ vars/runSemgrepScan.groovy
@@ -2,6 +2,6 @@ def call(String targetDir = '', String outputFile = 'semgrep_report.json') {
     sh """
         docker run --rm -v /home/kyarnk/JenkinsSecurity:/mnt semgrep/semgrep semgrep --config=auto /mnt/JenkinsSecurity/juice-shop -o /mnt/reports/${outputFile}
     """
-    echo "Semgrep scan completed. Report saved to /var/lib/jenkins/reports/${outputFile}"
+    echo "Semgrep scan completed. Report saved to /home/kyarnk/reports/${outputFile}"
 }
 
