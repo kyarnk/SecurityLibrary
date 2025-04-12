a7fbfb73ca4cbe7dacfd5335fb2f0887320db8e0
Nikita Kyarnonen <nk47blue@gmail.com>
2025-04-12 15:30:10 +0000

    kics added

--- vars/runKICSScan.groovy
+++ vars/runKICSScan.groovy
@@ -1,6 +1,29 @@
-def call(String targetDir = '', String outputFile = 'kics_report.json') {
+// def call(String targetDir = '', String outputFile = 'kics_report.json') {
+//     sh """
+//         docker run --rm -v ${targetDir}:/path checkmarx/kics:latest scan -p /path -o /path/${outputFile}
+//     """
+//     echo "KICS scan completed. Report saved to ${targetDir}/${outputFile}"
+// }
+
+def call(String targetDir = '', String outputFile = 'kics_report.json', String homeDir = '', String workspaceDir = '') {
+    // Проверяем, если homeDir не передан, использовать пустую строку
+    if (homeDir == '') {
+        error "Home directory must be specified!"
+    }
+    
+    // Если workspaceDir не указан, используем $WORKSPACE (автоматический параметр Jenkins)
+    workspaceDir = workspaceDir ?: env.WORKSPACE
+    
+    // Директория для отчётов
+    def outputDir = "${homeDir}/reports"
+
+    // Создаём директорию для отчётов, если её нет
+    sh "mkdir -p ${outputDir}"
+
+    // Запуск KICS с использованием Docker
     sh """
         docker run --rm -v ${targetDir}:/path checkmarx/kics:latest scan -p /path -o /path/${outputFile}
     """
-    echo "KICS scan completed. Report saved to ${targetDir}/${outputFile}"
+    
+    echo "KICS scan completed. Report saved to ${outputDir}/${outputFile}"
 }
