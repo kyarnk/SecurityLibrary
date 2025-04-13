508639bc9dc76b8d16a106888bb10a6b5de824a5
Nikita Kyarnonen <nk47blue@gmail.com>
2025-04-13 13:05:05 +0000

    added nuclei scan

--- vars/runNucleiScan.groovy
+++ vars/runNucleiScan.groovy
@@ -1,6 +1,23 @@
-def call(String targetUrl = 'https://juiceshop.kyarnk.ru', String outputFile = 'nuclei_report.json') {
+// def call(String targetUrl = 'https://juiceshop.kyarnk.ru', String outputFile = 'nuclei_report.json') {
+//     sh """
+//         docker run --rm -v /home/kyarnk/nuclei-reports:/output projectdiscovery/nuclei:latest -u ${targetUrl} -o /output/${outputFile}
+//     """
+//     echo "Nuclei scan completed. Report saved to /home/kyarnk/nuclei-reports/${outputFile}"
+// }
+
+def call(String targetUrl = '', String outputFile = 'nuclei_report.json', String homeDir = '') {
+    if (!targetUrl || !homeDir) {
+        error "Both targetUrl and homeDir must be specified!"
+    }
+
+    def outputDir = "${homeDir}/reports"
+    sh "mkdir -p ${outputDir}"
+
+    // Запуск Nuclei
     sh """
-        docker run --rm -v /home/kyarnk/nuclei-reports:/output projectdiscovery/nuclei:latest -u ${targetUrl} -o /output/${outputFile}
+        docker run --rm -v ${outputDir}:/output projectdiscovery/nuclei:latest \
+        -u ${targetUrl} -json -o /output/${outputFile}
     """
-    echo "Nuclei scan completed. Report saved to /home/kyarnk/nuclei-reports/${outputFile}"
+
+    echo "Nuclei scan completed. Report saved to ${outputDir}/${outputFile}"
 }
